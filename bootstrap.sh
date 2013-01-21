921738d8e0d8bca0c411e98e78902dcc021b2ee1
Johnathan Botha <jokillsya@gmail.com>
2013-01-21 12:21:57 +0200

    Added gitignore entries and make bootstrap a bit more portable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
-libtoolize \
-&& aclocal \
+if hash libtoolize 2>&-
+then
+  libtoolize --automake
+else
+  glibtoolize --automake
+fi
+
+aclocal \
 && automake --gnu --add-missing \
 && autoconf
